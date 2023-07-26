810bfd575d2ff75fa8cf362c82f8b6edc2ba29c5
Zahid Hasan Juel <83463788+Zahid-H@users.noreply.github.com>
2023-07-26 20:24:41 +0600

    Maven project, Maven dependency and TestNG created

---
+++ us_bangla_autoamtion/src/main/java/usbangla/us_bangla_autoamtion/App.java
@@ -0,0 +1,13 @@
+package usbangla.us_bangla_autoamtion;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
