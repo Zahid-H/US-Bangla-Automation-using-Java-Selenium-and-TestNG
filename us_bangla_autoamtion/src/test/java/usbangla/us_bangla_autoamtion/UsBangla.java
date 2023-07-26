5a93261cada8f4b98b6e441b3a3a71277a481515
Zahid Hasan Juel <83463788+Zahid-H@users.noreply.github.com>
2023-07-26 20:30:25 +0600

    Writing after method "@AfterMethod" to quit driver

--- us_bangla_autoamtion/src/test/java/usbangla/us_bangla_autoamtion/UsBangla.java
+++ us_bangla_autoamtion/src/test/java/usbangla/us_bangla_autoamtion/UsBangla.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class UsBangla {
@@ -23,5 +24,11 @@ public class UsBangla {
 	driver.manage().window().maximize();
 	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
+	}
+
+    @AfterMethod
+	public void tearDown()
+	{
+	driver.quit();
 	}
 }
